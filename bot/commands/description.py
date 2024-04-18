a04ae78c20e1d6f8a963b3a5c285b38b48ad6296
Gefferson Casasola <gcasasolah@uni.pe>
2024-04-18 16:42:44 -0500

    Add download images

--- bot/commands/description.py
+++ bot/commands/description.py
@@ -9,18 +9,19 @@ from telegram.ext import (
 from common.log import logger
 from bot.service import change_html_to_text
 
-EXCEL_FILE = range(1)
+DESCRIPTION_EXCEL_FILE = range(1)
 
 
 async def start_description(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Starts the conversation and asks excel file with description."""
+    user_name = update.effective_user.first_name
     await update.message.reply_text(
-        "Hi! My name is Gef Bot. I will hold a conversation with you. "
+        f"Hi {user_name}. I will hold a conversation with you. "
         "Send /cancel_des to stop talking to me.\n\n"
         "Please, send me the Excel file with descriptions of up to 20MB in size."
     )
 
-    return EXCEL_FILE
+    return DESCRIPTION_EXCEL_FILE
 
 
 async def excel_file(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -30,7 +31,7 @@ async def excel_file(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     ):
         await update.message.reply_text("Please send an Excel file.")
-        return EXCEL_FILE
+        return DESCRIPTION_EXCEL_FILE
     
     new_file = await update.message.effective_attachment.get_file()
     await new_file.download_to_drive("./excel-files/descriptions/description-html.xlsx")
@@ -60,7 +61,7 @@ async def cancel_description(update: Update, context: ContextTypes.DEFAULT_TYPE)
 description_conv_handler = ConversationHandler(
     entry_points=[CommandHandler("start_des", start_description)],
     states={
-        EXCEL_FILE: [MessageHandler(filters.ATTACHMENT, excel_file)],
+        DESCRIPTION_EXCEL_FILE: [MessageHandler(filters.ATTACHMENT, excel_file)],
     },
     fallbacks=[CommandHandler("cancel_des", cancel_description)],
 )
--- main.py
+++ main.py
@@ -11,18 +11,18 @@ from telegram.ext import (
 from bot.handlers import error_handler, text_handler, unknown_command
 from bot.commands.ean import ean_conv_handler
 from bot.commands.description import description_conv_handler
+from bot.commands.image import download_img_conv_handler
 
 
 def add_handlers(dp):
     dp.add_handler(CommandHandler("menu", text_handler))
     dp.add_handler(ean_conv_handler)
     dp.add_handler(description_conv_handler)
+    dp.add_handler(download_img_conv_handler)
     dp.add_error_handler(error_handler)
     dp.add_handler(MessageHandler(filters.COMMAND, unknown_command))
 
-
 add_handlers(ptb)
-
 
 if __name__ == "__main__":
     if config.DEBUG == "True":
